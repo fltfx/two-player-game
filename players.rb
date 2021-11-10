bea4ff6a95e8a804345e101fdcbb1e301b66217c
CYW <cyw@CYWs-MacBook-Pro.local>
2021-11-09 17:00:58 -0700

    init

---
+++ players.rb
@@ -0,0 +1,18 @@
+class players
+  attr_accessor :id
+
+  def initialize(id) #new initialize
+    @id = id
+  end
+
+  #update score
+  
+  #outputting score
+
+  #current player
+
+  #input answer
+
+  #method: check answer
+
+end
